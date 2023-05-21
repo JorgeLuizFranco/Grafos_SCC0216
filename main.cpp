b3d115416cb25ebb075791f18dce7ed89378e90e
Arthur Queiroz <arthurquimu@gmail.com>
2023-05-21 01:47:46 -0300

    Organize more

--- main.cpp
+++ main.cpp
@@ -13,35 +13,37 @@
 
 using namespace std;
 
+// Para armazenar as coordenadas, ficou mais fácil utilizar um pair<int, int>
 using pii = pair<int, int>;
 
-void update_move(pii mov_son, pii mov_parent, int &num_steps_, int &moves_up_,
-                 int &moves_down_, int &moves_left_, int &moves_right_);
-
-void print_solution(const vector<vector<pii>> &parent, pii node_solution);
+// 
+void printa_solucao(const vector<vector<pii>> &pai, pii node_solution);
 
 vector<pii> vizinhos(pii no_atual);
 
-bool is_in_limits(pii node, int n)
+bool no_grid(pii node, int n)
 
-void update_move(pii mov_son, pii mov_parent, int &num_steps_, int &moves_up_,
-                 int &moves_down_, int &moves_left_, int &moves_right_);
+vector<pii> cria_posicoes_fantasma(const vector<char> &movimentos_fantasma, pii inicio_fantasma, int n)
 
-void bfs_queue(const vector<vector<int>> &grid, vector<vector<int>> &dist,
-               vector<vector<pii>> &parent, pii inicio_pacman, int n);
+void atualiza_movimento(pii mov_filho, pii mov_pai, int &num_passos_, int &movimentos_cima_,
+                 int &movimentos_baixo_, int &movimentos_esq_, int &movimentos_dir_);
 
-void solve(const vector<vector<int>> &grid,
+void bfs(const vector<vector<int>> &grid, vector<vector<int>> &dist,
+               vector<vector<pii>> &pai, pii inicio_pacman, int n);
+
+void resolve(const vector<vector<int>> &grid,
            const vector<char> &movimentos_fantasma, pii inicio_fantasma,
            pii inicio_pacman, int n);
 
 int main() {
-  int n, m;
+  int n, m; // dimensao do tabuleiro, num de movimentos do fantasma
   cin >> n;
 
-  pii inicio_fantasma, inicio_pacman;
+  pii inicio_fantasma, inicio_pacman; // (coordenadas)
   vector<char> movimentos_fantasma;
   vector<vector<int>> grid(n, vector<int>(n, 0));  // inicializo o vetor de grid
 
+  // le o vetor de grid
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
       cin >> grid[i][j];
@@ -58,20 +60,20 @@ int main() {
     movimentos_fantasma.push_back(direcao);
   }
 
-  solve(grid, movimentos_fantasma, inicio_fantasma, inicio_pacman, n);
+  resolve(grid, movimentos_fantasma, inicio_fantasma, inicio_pacman, n);
 
   return 0;
 }
 
 
-void print_solution(const vector<vector<pii>> &parent, pii node_solution) {
+void printa_solucao(const vector<vector<pii>> &pai, pii node_solution) {
   int numero_passos = 0, movimentos_cima = 0, movimentos_baixo = 0,
       movimentos_esquerda = 0, movimentos_direita = 0;
   auto [i, j] = node_solution;
 
-  while (parent[i][j] != pii(-1, -1)) {
-    pii father = parent[i][j];
-    update_move({i, j}, father, numero_passos, movimentos_cima,
+  while (pai[i][j] != pii(-1, -1)) {
+    pii father = pai[i][j];
+    atualiza_movimento({i, j}, father, numero_passos, movimentos_cima,
                 movimentos_baixo, movimentos_esquerda, movimentos_direita);
     i = father.first, j = father.second;
   }
@@ -92,7 +94,7 @@ vector<pii> vizinhos(pii no_atual) {
   return nodes;
 }
 
-bool is_in_limits(pii node, int n) {
+bool no_grid(pii node, int n) {
   auto [i, j] = node;
   return i >= 0 && i < n && j >= 0 && j < n;
 }
@@ -115,7 +117,7 @@ vector<pii> cria_posicoes_fantasma(const vector<char> &movimentos_fantasma, pii 
                   posicao_atual = {posicao_atual.first - 1, posicao_atual.second};
         break;
       case 'D': if (posicao_atual.first<n-1)
-        posicao_atual = {posicao_atual.first + 1, posicao_atual.second};
+                  posicao_atual = {posicao_atual.first + 1, posicao_atual.second};
         break;
     }
     posicoes_fantasma.push_back(posicao_atual);
@@ -124,8 +126,8 @@ vector<pii> cria_posicoes_fantasma(const vector<char> &movimentos_fantasma, pii 
   return posicoes_fantasma;
 }
 
-void bfs_queue(const vector<vector<int>> &grid, vector<vector<int>> &dist,
-               vector<vector<pii>> &parent, pii inicio_pacman, int n) {
+void bfs(const vector<vector<int>> &grid, vector<vector<int>> &dist,
+               vector<vector<pii>> &pai, pii inicio_pacman, int n) {
   queue<pair<pii, int>> fila;  // fila que guarda {posicao,dist}
   fila.push({inicio_pacman,0});
   dist[inicio_pacman.first][inicio_pacman.second] = 0;
@@ -135,27 +137,30 @@ void bfs_queue(const vector<vector<int>> &grid, vector<vector<int>> &dist,
     fila.pop();
 
     for (auto [i, j] : vizinhos(no)) {
-      if (is_in_limits({i, j}, n) and
+      if (no_grid({i, j}, n) and
           dist[i][j] == -1 and grid[i][j]!=1) {         // se estiver nos limites e não visitado
         dist[i][j] = dist_atual + 1;  //  atualizo a distância
-        parent[i][j] = no;
+        pai[i][j] = no;
         fila.push({pii(i, j), dist[i][j]});  // coloco na fila
       }
     }
   }
 }
 
-void solve(const vector<vector<int>> &grid,
+void resolve(const vector<vector<int>> &grid,
            const vector<char> &movimentos_fantasma, pii inicio_fantasma,
            pii inicio_pacman, int n) {
+  
   vector<pii> posicoes_fantasma =
       cria_posicoes_fantasma(movimentos_fantasma, inicio_fantasma, n);
 
+  
+  /* seto a matriz de distâncias para -1, pois assim podemos usa la como vetor
+   * de visitados também */
   vector<vector<int>> dist(n, vector<int>(n, -1));
-  vector<vector<pii>> parent(n, vector<pii>(n, pii(-1, -1)));
-  // seto a matriz de distâncias para -1, pois assim podemos usa la como vetor
-  // de visitados também
-  bfs_queue(grid, dist, parent, inicio_pacman, n);
+  vector<vector<pii>> pai(n, vector<pii>(n, pii(-1, -1))); // pai[i][j] armazena de onde veio (i, j) na bfs
+
+  bfs(grid, dist, pai, inicio_pacman, n);
 
   int min_dist = n * n + 2;
   pii min_pos;
@@ -165,8 +170,7 @@ void solve(const vector<vector<int>> &grid,
     if (dist[x][y] <= i and min_dist > dist[x][y] and dist[x][y]!=-1) {
       min_dist = dist[x][y];
       min_pos = {x, y};
-    }
-    else if(i==n_posicoes-1 and dist[x][y]!=-1){
+    } else if (i == n_posicoes-1 and dist[x][y] != -1){
       min_dist = dist[x][y];
       min_pos = {x, y};
     }
@@ -177,25 +181,25 @@ void solve(const vector<vector<int>> &grid,
     return;
   }
 
-  print_solution(parent,min_pos);
+  printa_solucao(pai,min_pos);
 }
 
-void update_move(pii mov_son, pii mov_parent, int &num_steps_, int &moves_up_,
-                 int &moves_down_, int &moves_left_, int &moves_right_) {
-  auto [i1, j1] = mov_son;
-  auto [i2, j2] = mov_parent;
+void atualiza_movimento(pii mov_filho, pii mov_pai, int &num_passos_, int &movimentos_cima_,
+                 int &movimentos_baixo_, int &movimentos_esq_, int &movimentos_dir_) {
+  auto [i1, j1] = mov_filho;
+  auto [i2, j2] = mov_pai;
   int di = i2 - i1;
   int dj = j2 - j1;
 
   if (di == 1) {
-    moves_up_++;
+    movimentos_cima_++;
   } else if (di == -1) {
-    moves_down_++;
+    movimentos_baixo_++;
   } else if (dj == 1) {
-    moves_left_++;
+    movimentos_esq_++;
   } else if (dj == -1) {
-    moves_right_++;
+    movimentos_dir_++;
   }
 
-  num_steps_++;
+  num_passos_++;
 }
